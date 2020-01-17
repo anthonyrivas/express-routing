951ab6f0b2a2d8480799edfe029d4d41b56ac808
Anthony Rivas <anthony.edward.rivas@gmail.com>
2020-01-16 18:46:08 -0700

    Basic express setup

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware goes here
-
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
