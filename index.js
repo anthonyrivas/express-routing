a63d8efd8ef131f8aa7594b512cd2e9a0e4de853
Anthony Rivas <anthony.edward.rivas@gmail.com>
2020-01-16 19:07:54 -0700

    Routing in server file

--- index.js
+++ index.js
@@ -2,13 +2,49 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const data = [{
+  id: 1,
+  post: "Hello"
+},
+{
+  id: 2,
+  post: "There"
+},
+{
+  id: 3,
+  post: "The angel"
+},
+{
+  id: 4,
+  post: "from my nightmare"
+}];
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 // Routes go here
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the jungle.');
+});
 
+app.post('/', (req, res) => {
+  const {username, password} = req.body;
+  if (username === 'jsmith' && password === 'I wanna rock!') {
+    res.send(`Welcome John, I wanna rock too!`);
+  }
+  res.send(`Sorry, I don't know that user`);
+});
+
+app.get('/posts', (req, res) => {
+  res.json(data);
+});
+
+app.get('/posts/:postId', (req, res) => {
+  const {postId} = req.params;
+  const postSearch = data.filter((v) => v.id === parseInt(postId));
+  const post = postSearch.length > 0 ? postSearch[0] : {error: 'Not found'};
+  res.send(post);
+});
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
